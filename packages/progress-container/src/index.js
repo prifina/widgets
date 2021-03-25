08925310fd153f728c34ab1654ca7f397b2cd045
Jennifer Kim <jennkim@Jennifers-MacBook-Pro.local>
2021-03-25 16:10:08 -0400

    Modified

--- packages/progress-container/src/index.js
+++ packages/progress-container/src/index.js
@@ -1,11 +1,13 @@
 /**
  * Entrypoint of the Remote Component.
  */
-//import { App } from "./App";
+
+ import { App } from "./App";
 
 //export default App;
 // import Hello from "./Hello";
 // export default Hello;
 
-import PC from "./PC";
-export default PC; 
+export default App;
+// import PC from "./PC";
+// export default App; 
