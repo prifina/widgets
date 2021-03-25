026e2860252960e521347d1649be42567c08fe31
Jennifer Kim <jennkim@Jennifers-MacBook-Pro.local>
2021-03-25 12:55:01 -0400

    App.stories.js origianl

--- packages/progress-container/stories/App.stories.js
+++ packages/progress-container/stories/App.stories.js
@@ -1,52 +1,10 @@
-import React from "react";
-import { PrifinaProvider, usePrifina } from "@prifina/hooks";
-
-export default { title: "ProviderTest" };
-
-export const providerTest = () => {
-  const { currentUser, check } = usePrifina({});
-  console.log(currentUser);
-
-  return (
-    <div>
-      Provider Testing{" "}
-      <button
-        onClick={() => {
-          console.log(check());
-        }}
-      >
-        Test
-      </button>
-    </div>
-  );
-};
-/*
-providerTest.story = {
-  name: "Provider Test",
-};
-*/
-
-providerTest.story = {
-  name: "Provider Test",
-  decorators: [
-    (Story) => {
-      return (
-        <PrifinaProvider>
-          <Story />
-        </PrifinaProvider>
-      );
-    },
-  ],
-};
-
-/*
 import React, { useState, useEffect } from "react";
-import App from "../src/PC";
+import App from "../src/Hello";
 
 export default { title: "Test" };
 
 export const app = () => {
-  
+  /*
   const [onUpdate, set] = useState({});
   useEffect(() => {
     let timer = null;
@@ -57,11 +15,10 @@ export const app = () => {
 
     return () => clearTimeout(timer);
   }, []);
-  
+  */
 
   return <App msg={"Hello, Tero"} />;
 };
 app.story = {
   name: "App",
 };
-*/
