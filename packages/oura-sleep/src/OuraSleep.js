b9725d3146e0a82b450b300c7e20617e97db06e0
hamza <hamzahalilovic.12@gmail.com>
2022-09-05 14:41:42 +0300

    b

--- packages/oura-sleep/src/OuraSleep.js
+++ packages/oura-sleep/src/OuraSleep.js
@@ -116,12 +116,7 @@ const OuraSleep = (props) => {
     const d = new Date();
     const dd = d.setDate(d.getDate() - day);
 
-    console.log("day", d);
-    console.log("day dd", dd);
-
     const dateStr = new Date(dd).toISOString().split("T")[0];
-
-    console.log("date str", dateStr);
 
     setDate(dateStr);
 
@@ -144,7 +139,7 @@ const OuraSleep = (props) => {
     }
   }, [day]);
 
-  console.log("day state", day);
+  console.log("day", day);
 
   return (
     <Container>
