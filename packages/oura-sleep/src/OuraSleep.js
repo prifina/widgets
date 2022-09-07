97e73a2f745ad87f4271970c9fb9285fda2307c8
hamza <hamzahalilovic.12@gmail.com>
2022-09-07 05:41:31 +0300

    b

--- packages/oura-sleep/src/OuraSleep.js
+++ packages/oura-sleep/src/OuraSleep.js
@@ -43,10 +43,58 @@ const Container = styled.div`
 // unique appID for the widget....
 const appID = "6dyqsLq4MEJC2sT9WNBGUs";
 
+let falseData = [
+  {
+    summary_date: "2022-09-04",
+    period_id: 0,
+    is_longest: 1,
+    timezone: 0,
+    bedtime_start: "2022-09-04T22:52:00+00:00",
+    bedtime_end: "2022-09-05T03:55:00+00:00",
+    score: 62,
+    score_total: 57,
+    score_disturbances: 83,
+    score_efficiency: 99,
+    score_latency: 88,
+    score_rem: 97,
+    score_deep: 59,
+    score_alignment: 31,
+    total: 16891,
+    duration: 18180,
+    awake: 1289,
+    light: 11655,
+    rem: 2514,
+    deep: 2722,
+    onset_latency: 480,
+    restless: 39,
+    efficiency: 94,
+    midpoint_time: 11010,
+    hr_lowest: 49,
+    hr_average: 56.375,
+    rmssd: 54,
+    breath_average: 13,
+    temperature_delta: -0.06,
+    hypnogram_5min:
+      "443432222211222333321112222222222111133333322221112233333333332232222334",
+    hr_5min: [
+      0, 53, 51, 0, 50, 50, 49, 49, 50, 50, 51, 52, 52, 51, 53, 58, 60, 60, 59,
+      58, 58, 58, 58, 55, 55, 55, 55, 56, 56, 55, 53, 53, 53, 53, 53, 53, 57,
+      58, 60, 60, 59, 57, 59, 58, 56, 56, 56, 56, 55, 55, 56, 56, 57, 58, 55,
+      56, 57, 60, 58, 58, 59, 57, 54, 54, 53, 52, 52, 55, 53, 54, 56, 0,
+    ],
+    rmssd_5min: [
+      0, 0, 62, 0, 75, 52, 56, 56, 64, 57, 55, 78, 77, 83, 70, 35, 21, 25, 49,
+      44, 48, 48, 62, 69, 66, 64, 79, 59, 67, 66, 70, 63, 53, 57, 53, 57, 38,
+      26, 18, 24, 30, 35, 36, 46, 53, 59, 50, 50, 53, 53, 57, 52, 41, 37, 49,
+      47, 48, 35, 32, 34, 52, 57, 62, 57, 70, 81, 81, 65, 69, 72, 64, 0,
+    ],
+  },
+];
+
 const OuraSleep = (props) => {
   const { onUpdate, Prifina, API, registerHooks } = usePrifina();
 
-  const [processedData, setProcessedData] = useState();
+  const [processedData, setProcessedData] = useState([]);
 
   const processData = (data) => {
     console.log("ORIGINAL PROCESS DATA", data);
@@ -55,7 +103,7 @@ const OuraSleep = (props) => {
 
     console.log("newData", newData);
 
-    setProcessedData(newData);
+    setProcessedData(data);
   };
 
   console.log("processed data", processedData);
@@ -124,10 +172,11 @@ const OuraSleep = (props) => {
 
     console.log("THE NEW BUILD result", result);
 
-    if (stage === "dev") {
-      processData(result.data.getDataObject.content[1].score[1]);
-    }
     processData(result.data.getDataObject.content[0]);
+
+    // if (stage === "dev") {
+    //   processData(result.data.getDataObject.content[1].score[1]);
+    // }
   }, [day]);
 
   console.log("day", day);
@@ -147,10 +196,16 @@ const OuraSleep = (props) => {
           alignItems="center"
           bg="#FFA654"
           padding="0px 65px 0px 65px"
-          borderTopRightRadius={10}
-          borderTopLeftRadius={10}
+          borderTopRightRadius={8}
+          borderTopLeftRadius={8}
         >
           <IconButton
+            style={{
+              background: "transparent",
+              border: 0,
+              cursor: "pointer",
+              fontSize: 19,
+            }}
             aria-label="Search database"
             icon={<ChevronLeftIcon />}
             onClick={async () => {
@@ -160,6 +215,12 @@ const OuraSleep = (props) => {
           <Text>{date}</Text>
 
           <IconButton
+            style={{
+              background: "transparent",
+              border: 0,
+              cursor: "pointer",
+              fontSize: 19,
+            }}
             aria-label="Search database"
             icon={<ChevronRightIcon />}
             onClick={async () => {
@@ -179,8 +240,8 @@ const OuraSleep = (props) => {
           <Box
             height={202}
             style={{
-              // background: "#FBF2F2",
-              opacity: 1,
+              background: "rgba(0255, 255, 255, 0.1)",
+              // opacity: 0.5,
               borderBottomLeftRadius: 10,
               borderBottomRightRadius: 10,
             }}
@@ -202,10 +263,10 @@ const OuraSleep = (props) => {
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="awake" stackId="a" fill="#FFE9D5" />
-                <Bar dataKey="light" stackId="a" fill="#FFA654" />
-                <Bar dataKey="deep" stackId="a" fill="#B96314" />
-                <Bar dataKey="rem" stackId="a" fill="#6D3D10" />
+                <Bar dataKey="awake" fill="#FFE9D5" />
+                <Bar dataKey="light" fill="#FFA654" />
+                <Bar dataKey="deep" fill="#B96314" />
+                <Bar dataKey="rem" fill="#6D3D10" />
               </BarChart>
             </ResponsiveContainer>
           </Box>
